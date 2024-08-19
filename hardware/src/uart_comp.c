7f5d9e02260d2e4740df618d9e829a3611e88ebc
Michael Artuerhof <michael.artuerhof@gmail.com>
2024-08-19 15:20:02 +0300

    - removed compiler's warnings

--- hardware/src/uart_comp.c
+++ hardware/src/uart_comp.c
@@ -3,5 +3,9 @@
 // does nothing
 HAL_StatusTypeDef WRP_UART_Receive_IT(UART_HandleTypeDef * huart, uint8_t * data, uint16_t size)
 {
+    // remove compiler's warnings
+    (void)huart;
+    (void)data;
+    (void)size;
     return 0;
 }
